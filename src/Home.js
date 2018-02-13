a57bd0672be438cf4470df40c0bde7ebb44c8648
mohan <venugopal@enroco.com>
2018-02-13 13:07:45 +0530

    Modified paragraph in home.js

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
           The team saw it's greatest period in the 1980s and much of the success can be owed to the appearance of stellar players, that are now living legends, like Sunil Gavaskar, Kapil Dev, Erapalli Prasanna, Srinivas Venkataraghavan, Bishen Singh Bed etc.
           The Indian cricket team is currently ranked third by the ICC in Tests, first in ODIs and third in T20Is.
           India' first World Cup victory was in 1983 under Kapil Dev. India won the World Cup a second time in 2011 under Mahendra Singh Dhoni. India also won the inaugural World Twenty20 under the captaincy of Dhoni in 2007. Dhoni has proven to be a captain par excellence over the years. Under the leadership of Dhoni,
-          the Indian team has set a national record for most back-to-back ODI wins, with nine to their credit.</p>
+          the Indian team has set a national record.</p>
       </div>
       );
   }
